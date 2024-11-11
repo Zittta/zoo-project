e5e3f73a11c5a6e048298b02d32514bc3e58104f
Zittta <95086761+Zittta@users.noreply.github.com>
2024-11-11 12:20:08 +0700

    Update zoo.py

--- zoo.py
+++ zoo.py
@@ -6,7 +6,7 @@ class Zoo:
             return 100
         elif 21 <= age <= 60:
             return 150
-        elif age >=60:
+        elif age >60:
             return 100
         else:
             return "invalid"
